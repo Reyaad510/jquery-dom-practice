d2c1b02bd194a916a5ea08448954a4101fcce7d0
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-24 22:49:20 -0500

    Part One Completed

--- app.js
+++ app.js
@@ -6,3 +6,36 @@ $("article img").addClass("image-center");
 
 // 3 -> Remove last paragraph in article
 $("article p:last").remove();
+
+// 4
+let randonNum = Math.floor(Math.random() * 100 + 1);
+$("#title").css("font-size", `${randonNum}px`);
+
+//5
+$("ol").append("<li>I like Bob's Burger!</li>");
+
+//6
+$("aside")
+  .empty()
+  .append(
+    "<p>I am so sorry for that list. It was not the best I must admit...</p>"
+  );
+
+//7
+function updateBackgroundColor() {
+  let redColor = $("input").eq(0).val();
+  let greenColor = $("input").eq(1).val();
+  let blueColor = $("input").eq(2).val();
+
+  $("body").css(
+    "background-color",
+    `rgb(${redColor},${greenColor}, ${blueColor} )`
+  );
+}
+
+$("input").on("input", updateBackgroundColor);
+
+// 8
+$("img").on("click", function () {
+  $(this).remove();
+});
