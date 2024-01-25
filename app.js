34eb6a8e9ca655315979793c06e670447196639c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-24 21:55:23 -0500

    Completed Part One Questions 1,2,3

---
+++ app.js
@@ -0,0 +1,8 @@
+// 1
+console.log("Let's get ready to party with jQuery!");
+
+// 2 -> Add class called "image-center"
+$("article img").addClass("image-center");
+
+// 3 -> Remove last paragraph in article
+$("article p:last").remove();
